853163af079e51f7f136d425331fde1c01705786
Truong Vinh Phuoc <truongphuocst310301@gmail.com>
2023-05-27 00:06:36 +0700

    skeleton for RiverML model

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
 from flask import Flask, jsonify
 import os
-
+import river
+import pickle
+from river import reco
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
     return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+
+
+@app.route('/save_model')
+def saveModel():
+    model = reco.BiasedMF()
+    with open('MF_model.pkl', 'wb') as f:
+        pickle.dump(model, f)
+
+    return 'Model saved'
+
+
+@app.route('/read_model')
+def readModel():
+    with open('MF_model.pkl', 'rb') as f:
+        model = pickle.load(f)
+        return 'Readed model'
 
 
 @app.route('/save_file/<content>')
